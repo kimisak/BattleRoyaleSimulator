add45af310a7f23c4074485fa97c30e78cfa16f2
Kim Isak Olsen <40180952+kimisak@users.noreply.github.com>
2023-10-29 00:07:20 +0200

    Draft frontpage for larger screens

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,35 @@
+<div class="grid grid-cols-8 grid-rows-6 h-screen w-screen">
+	<aside class="col-span-1 row-span-full bg-black text-white text-center">
+		<img src="../../images/goatgames_3.png" alt="logo" />
+		<h1>Battle Royale Simulator</h1>
+		<h2>Timeline</h2>
+	</aside>
+	<header
+		class="col-start-2 col-span-full bg-black text-white items-end flex justify-between gap-x-20"
+	>
+		<div class="flex gap-x-20">
+			<nav class="flex gap-x-5 ml-5">
+				<a href="/">Home</a>
+				<a href="/tutorial">How to Play</a>
+				<a href="/settings">Settings</a>
+			</nav>
+			<nav class="flex gap-x-5">
+				<a href="/characters">Characters</a>
+				<a href="/leaderboard">Leaderboard</a>
+				<a href="statistics">Statistics</a>
+			</nav>
+		</div>
+		<nav class="mr-5">
+			<a href="https://github.com/kimisak/battleroyalesimulator">GitHub</a>
+		</nav>
+	</header>
+	<main class="col-start-2 row-start-2 col-span-full row-span-full bg-gray-400">
+		<slot />
+	</main>
+</div>
+
+<style lang="postcss">
+	a:hover {
+		color: #de231c;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,11 @@
 <script lang="ts">
-	import TeamCard from '$lib/components/TeamCard.svelte';
 	import '../app.css';
 	import type { PageData } from './$types';
-
 	export let data: PageData;
 </script>
 
 <main>
-	<div class="p-10 flex flex-col sm:flex-row sm:flex-wrap gap-10 w-full justify-center">
-		{#each data.teams as team}
-			<TeamCard {team} />
-		{/each}
-	</div>
+	<h1>Hello</h1>
 </main>
 
 <style lang="postcss">
